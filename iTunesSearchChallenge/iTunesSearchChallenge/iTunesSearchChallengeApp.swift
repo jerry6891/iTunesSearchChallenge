eb4ca6beba1eae4a695b900afa4e56ab9d47a179
Jerry Lozano <glozano@shopnet.com.mx>
2022-10-06 19:13:13 -0500

    Commit Initialization

---
+++ iTunesSearchChallenge/iTunesSearchChallenge/iTunesSearchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchChallengeApp.swift
+//  iTunesSearchChallenge
+//
+//  Created by Jerry Lozano on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
